602a1cb4a8de0a2e294ba1eb42c9b21593d07f32
Brian Forth <brianwforth@gmail.com>
2021-05-14 18:52:20 -0500

    Corrected issues with schema and ERD after cleaning review data

--- ERD.sql
+++ ERD.sql
@@ -17,19 +17,11 @@ CREATE TABLE "products" (
 );
 
 CREATE TABLE "reviews" (
-    "brand" varchar   NOT NULL,
     "key" varchar   NOT NULL,
-    "author" varchar   NOT NULL,
-    "date" datetime   NOT NULL,
     "stars" int   NOT NULL,
-    "title" varchar   NOT NULL,
     "helpful_yes" int   NOT NULL,
     "helpful_no" int   NOT NULL,
-    "text" varchar   NOT NULL,
-    "taste" int   NOT NULL,
-    "ingredients" int   NOT NULL,
-    "texture" int   NOT NULL,
-    "likes" int   NOT NULL
+    "text" varchar   NOT NULL
 );
 
 ALTER TABLE "reviews" ADD CONSTRAINT "fk_reviews_key" FOREIGN KEY("key")
--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 -- Creating ice cream tables
 CREATE TABLE products(
-	brand VARCHAR(10) NOT NULL,
-	key VARCHAR(5) NOT NULL,
-	name VARCHAR(40) NOT NULL,
+	brand VARCHAR(15) NOT NULL,
+	key VARCHAR(15) NOT NULL,
+	name VARCHAR(255) NOT NULL,
 	subhead VARCHAR(255),
-	description VARCHAR(1000),
+	description VARCHAR(10000),
 	rating FLOAT,
 	rating_count INT,
 	ingredients VARCHAR(10000),
@@ -12,19 +12,11 @@ CREATE TABLE products(
 );
 	
 CREATE TABLE reviews(
-	brand VARCHAR(10) NOT NULL,
-	key VARCHAR(5) NOT NULL,
-	author VARCHAR(40),
-	date VARCHAR(10),
+	key VARCHAR(15) NOT NULL,
 	stars INT,
-	title VARCHAR(100),
-	helpful_yes INT,
-	helpful_no INT,
-	text VARCHAR(1000),
-	taste INT,
-	ingredients INT,
-	texture INT,
-	likes INT,
+	helpful_yes FLOAT,
+	helpful_no FLOAT,
+	text VARCHAR(10000),
 	FOREIGN KEY (key) REFERENCES products(key)
 );
 
